fac2d6bf2b467357b4fe083d03f86b8c880d58d9
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2019-04-08 06:05:07 -0300

    Add short benchmark.

--- 2-primes/Foo.java
+++ 2-primes/Foo.java
@@ -2,12 +2,17 @@ import java.math.BigInteger;
 
 public class Foo {
     public static void main(String... args) {
+        var start = System.currentTimeMillis();
+
         var x = BigInteger.ONE;
 
         for (var i = 0; i < 100; i++) {
             x = x.multiply(new BigInteger(new byte[] {(byte)(i + 1)}));
         }
 
+        var end = System.currentTimeMillis();
+
         System.out.println("Factorial of 100 is " + x);
+        System.out.println("(" + (end - start) + "ms)");
     }
 }
--- 2-primes/foo.py
+++ 2-primes/foo.py
@@ -1,7 +1,13 @@
 from functools import reduce
+from datetime import datetime
 import operator
 
 if __name__ == '__main__':
+    start = datetime.now()
     x = reduce(operator.mul, (i + 1 for i in range(100)), 1)
+    end = datetime.now()
+
+    ellapsed = end - start
 
     print(f'Factorial of 100 is {x}')
+    print(f'({ellapsed.microseconds / 1000}ms)')
